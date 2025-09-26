dd02a27d207d4889f93829c9bbb96c94e89ba51a
nayonika04 <Nayonika078@gmail.com>
2025-09-26 14:42:37 +0530

    Update and rename DAY10_A_classify triangles.c to DAY10_A_classify_triangles.c

---
+++ DAY10_A_classify_triangles.c
@@ -0,0 +1,25 @@
+//Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+
+#include <stdio.h>
+
+int main() {
+    int side1, side2, side3;
+
+    printf("Enter the lengths of the three sides of the triangle:\n");
+    scanf("%d %d %d", &side1, &side2, &side3);
+
+    if ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)) {
+        if (side1 == side2 && side2 == side3) {
+            printf("The triangle is Equilateral.\n");
+        } else if (side1 == side2 || side2 == side3 || side1 == side3) {
+            printf("The triangle is Isosceles.\n");
+        } else {
+            printf("The triangle is Scalene.\n");
+        }
+    } else {
+        printf("The given side lengths do not form a valid triangle.\n");
+    }
+
+    return 0;
+}
+
