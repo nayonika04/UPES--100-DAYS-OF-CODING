0b82b52b544422db786e7b77657eef285fa4ccfe
nayonika04 <Nayonika078@gmail.com>
2025-09-26 14:32:04 +0530

    Update DAY05_A_simple_and_compound_interest.c

--- DAY05_A_simple and compound interest.c
+++ DAY05_A_simple and compound interest.c
@@ -29,23 +29,3 @@ int main() {
 
 }
 
-//Q10:Write a program to input time in seconds and convert it to hours:minutes:seconds format.
-#include<stdio.h>
-int main() {
-    int totalseconds;
-    int hours,minutes,seconds;
-
-    printf("enter the time in seconds:" );
-    scanf("%f", &totalseconds);
-
-    hours = totalseconds/3600;
-    int remainingseconds = totalseconds % 3600;
-
-    minutes = remainingseconds/ 60;
-    seconds = remainingseconds % 60;
-
-    printf("time in HH:MM:SS format: %d:%d:%d\n", hours, minutes, seconds);
-
-    return 0;
-
-}
